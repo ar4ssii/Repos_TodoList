085d76fb86eba82c2779f6e0dc5c455b7b5a6426
johnpaul <johnpaulk24@gmail.com>
2024-04-18 15:21:28 +0800

    John Paul Create the Add , delete and crossout function

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,65 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  var box = await Hive.openBox('database');
-
-  runApp(MaterialApp(home:MyApp()));
+  await Hive.openBox<String>('todos');
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
   @override
-  State<MyApp> createState() => _MyAppState();
-
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  var box = Hive.box('database');
-  void addData() async{
-
-    box.put('name', 'David');
-
-    print('Name: ${box.get('name')}');
-  }
+  late Box<String> todoBox;
 
   @override
   void initState() {
-    addData();
     super.initState();
+    todoBox = Hive.box('todos');
   }
 
-  final List<String> todoList = [];
   void addToDo() {
+    TextEditingController todoController = TextEditingController();
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Add Todo'),
-            content: TextField(
-              decoration: InputDecoration(labelText: 'Enter Todo'),
-              onSubmitted: (value) {
-                setState(() {
-                  todoList.add(value);
-                });
-
-                Navigator.pop(context);
-              },
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Todo'),
+          content: TextField(
+            controller: todoController,
+            decoration: InputDecoration(labelText: 'Enter Todo'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cancel'),
             ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
+            TextButton(
+              onPressed: () {
+                final text = todoController.text;
+                if (text.isNotEmpty) {
+                  final newTodo = Todo(description: text, isDone: false);
+                  todoBox.add(json.encode(newTodo.toJson()));
                   Navigator.pop(context);
-                },
-                child: Text('Cancel'),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    todoList.add('New Todo');
-                  });
-                  Navigator.pop(context);
-                },
-                child: Text('Add'),
-              ),
-            ],
-          );
-        },
+                }
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
     );
   }
 
+  void deleteToDo(int index) {
+    todoBox.deleteAt(index);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,105 +69,98 @@ class _MyAppState extends State<MyApp> {
         title: Text('ToDo List'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          // display this if there is no data
-          // Center(
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [
-          //       Text('No Data')
-          //     ],
-          //   ),
-          // ),
-
-          // item1
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      value: false, // Set your checkbox value here
-                      onChanged: (newValue) {
-                        // Handle checkbox onChanged event here
-                      },
+      body: ValueListenableBuilder(
+        valueListenable: todoBox.listenable(),
+        builder: (context, Box<String> box, _) {
+          if (box.values.isEmpty) {
+            return Center(child: Text("No Data"));
+          }
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              final todoJson = box.getAt(index);
+              if (todoJson != null) {
+                try {
+                  final todo = Todo.fromJson(json.decode(todoJson));
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                    SizedBox(width: 10),
-                    // Add spacing between Checkbox and Text
-                    Text(
-                      "${box.get('name')}",
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: todo.isDone,
+                              onChanged: (bool? newValue) {
+                                setState(() {
+                                  todo.isDone = newValue ?? false;
+                                  todoBox.putAt(index, json.encode(todo.toJson()));
+                                });
+                              },
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              todo.description,
+                              style: TextStyle(
+                                decoration: todo.isDone ? TextDecoration.lineThrough : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: () => deleteToDo(index),
+                          icon: Icon(Icons.delete),
+                          color: Colors.red,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {
-                    // Handle delete button onPressed event here
-                  },
-                  icon: Icon(Icons.delete),
-                  color: Colors.red, // Set delete button color
-                ),
-              ],
-            ),
-          ),
-
-          // item2
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      value: false, // Set your checkbox value here
-                      onChanged: (newValue) {
-                        // Handle checkbox onChanged event here
-                      },
-                    ),
-                    SizedBox(width: 10),
-                    // Add spacing between Checkbox and Text
-                    Text(
-                      "Todo 2",
-                    ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {
-                    // Handle delete button onPressed event here
-                  },
-                  icon: Icon(Icons.delete),
-                  color: Colors.red, // Set delete button color
-                ),
-              ],
-            ),
-          ),
-
-        ],
+                  );
+                } catch (e) {
+                  print("Error decoding todo at index $index: $e");
+                }
+              }
+              return SizedBox.shrink();
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: addToDo,
         tooltip: 'Add To Do',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class Todo {
+  String description;
+  bool isDone;
+
+  Todo({
+    required this.description,
+    required this.isDone,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'description': description,
+      'isDone': isDone,
+    };
+  }
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      description: json['description'],
+      isDone: json['isDone'],
     );
   }
 }
